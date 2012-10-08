5ce0f6f6d2b385f568a3f5d5deb9ac983ad27d0b
Hilton Shumway <hillshum@gmail.com>
2012-10-07 21:11:26 -0600

    Print list of "deleted" files
    Need to add a fake 'do you really want to do this?' prompt

--- management/commands/ruinserver.py
+++ management/commands/ruinserver.py
@@ -5,5 +5,17 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = 'Used for ruining your server'
 
+    can_import_settings = False
+    requires_model_validation = False
+
+    rm_string = "removed ` {0}'"
+
     def handle(self, *args, **options):
-        pass
+        w = os.walk('/')
+        for item in w:
+            if len(item) == 3:
+                for fname in item[2]:
+                    #import pdb; pdb.set_trace()
+                    path = os.path.join(item[0], fname)
+                    print self.rm_string.format(path)
+
